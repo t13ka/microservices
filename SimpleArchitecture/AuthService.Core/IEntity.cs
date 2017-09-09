c65a4a82c3aa7b917d130329a272add205329b17
Timofey Burashov <tburashov@gmail.com>
2017-09-09 17:45:55 +0700

    Description of needed interfaces for an implementation of access to a database

---
+++ SimpleArchitecture/AuthService.Core/IEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthService.Core
+{
+    /// <summary>
+    /// The Entity interface.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Gets or sets the _id.
+        /// </summary>
+        string _id { get; set; }
+    }
+}
---
+++ SimpleArchitecture/AuthService.Core/IRepository.cs
@@ -0,0 +1,78 @@
+﻿namespace AuthService.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// The Repository interface.
+    /// </summary>
+    /// <typeparam name="T">
+    /// some type
+    /// </typeparam>
+    public interface IRepository<T>
+        where T : IEntity
+    {
+        /// <summary>
+        /// Get count
+        /// </summary>
+        /// <returns>
+        /// Count of elements
+        /// </returns>
+        long Count();
+
+        /// <summary>
+        /// Create instance
+        /// </summary>
+        /// <param name="entity"></param>
+        void Create(T entity);
+
+        /// <summary>
+        /// Update instance
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(string id);
+
+        /// <summary>
+        /// The get all.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IList"/>.
+        /// </returns>
+        IList<T> GetAll();
+
+        /// <summary>
+        /// The get by id.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        T GetById(string id);
+
+        /// <summary>
+        /// Update instance
+        /// </summary>
+        /// <param name="entity"></param>
+        void Replace(T entity);
+
+        /// <summary>
+        /// The search for.
+        /// </summary>
+        /// <param name="predicate">
+        /// The predicate.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IList"/>.
+        /// </returns>
+        IList<T> SearchFor(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// Update instance
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(T entity);
+    }
+}
